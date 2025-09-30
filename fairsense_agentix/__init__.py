fe761e5b2975ddc8aa247bc8108fb6cada9d6041
Karanpal Sekhon <karanpal.sekhon@vectorinstitute.ai>
2025-09-29 21:27:48 -0400

    setup virtual environment + cleanup unneeded files

---
+++ fairsense_agentix/__init__.py
@@ -0,0 +1,1 @@
+"""FairSense-Agentix Module."""
---
+++ tests/fairsense-agentix/test_sanity.py
@@ -0,0 +1,6 @@
+"""Simple sanity test to ensure the testing framework is set up correctly."""
+
+
+# tests/test_sanity.py
+def test_sanity():
+    assert True
